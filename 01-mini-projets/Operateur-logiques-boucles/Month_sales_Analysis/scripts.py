169c30300b22508234ca71c10b01053d07eea839
sora <moussa.sissoko6@etu.univ-lorraine.fr>
2025-11-26 17:24:55 +0000

    feat: ajouter les scripts du projet

---
+++ 01-mini-projets/Operateur-logiques-boucles/Month_sales_Analysis/scripts.py
@@ -0,0 +1,63 @@
+# Listes de ref 
+  
+produits = ["Aspirateur", "Mixeur", "Casserole"]
+mois = ["janv", "Fév", "Mars", "Avr", "Mai", "Juin"]
+ventes_aspirateur = [100, 120, 230, 240, 1500, 1600]
+ventes_mixeur = [200, 300, 100, 120, 130, 300]
+ventes_casserole = [123, 1400, 474, 700, 560, 300]
+
+toutes_les_ventes = [ventes_aspirateur, ventes_mixeur, ventes_casserole]
+resultats_analyse = {}# dict vide afin de stocker les résultats agrégés pour chaque produit
+
+# Boucle principale
+
+print("*************** ANALYSES DES VENTES **********************")
+
+for i,  prd in enumerate(produits):
+    """ Afin d'obtenir l'indice i et le nom du produit pour l'iteration actuelle"""
+    ventes_actuelles = toutes_les_ventes[i]
+    total_ventes = 0
+    meilleure_vente_montant = -1 # Afin de s'assurer que le 1er mtnt soit enrégistré
+    meilleure_vente_mois = " "
+
+# Boucle interne 
+
+    for montant, nom_mois in zip(ventes_actuelles, mois):
+        """ Associe et traite le mntnt et le n.mois corrpdnd à chaque tour"""
+        # 1. Calcul du total des ventes
+
+        total_ventes += montant
+
+        # 2. Trouver le mois avec la meilleure vente
+
+        if montant > meilleure_vente_montant:
+            """
+            Si cette cond est vraie :
+            alors maj meilleure_vente_mtnt et
+             meilleure_ventes_mois par les valeurs actuelles
+             
+             """
+
+            meilleure_vente_montant = montant
+            meilleure_vente_mois = nom_mois
+    # Stockage des résultats dans le dic resultats analyse
+
+    resultats_analyse[prd] = {
+      "Total": total_ventes,
+      "Meilleur mois": meilleure_vente_mois,
+      "Montant Max":  meilleure_vente_montant
+    }
+
+# Afiichage des results
+
+for produit, donnees in resultats_analyse.items():
+    print(f"\nproduit : **{produit}**")
+    print(f" Ventes totales (6 mois) : {donnees['Total']} unités ")
+    print(f" Meilleures mois de vente: {donnees["Meilleur mois"]} ({donnees["Montant Max"]} unités)")
+
+
+
+
+
+
+
