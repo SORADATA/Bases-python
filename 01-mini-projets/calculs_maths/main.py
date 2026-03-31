fa30c93faca682f0e130ee94708af436cdcd09ac
sora <moussa.sissoko6@etu.univ-lorraine.fr>
2026-03-31 08:56:07 +0000

    feat(maths): Add Complex Number

--- 01-mini-projets/calculs_maths/main.py
+++ 01-mini-projets/calculs_maths/main.py
@@ -45,3 +45,32 @@ x=a*b//c
 y=a*(b//c)
 print("x=", x)
 print("x=", y)
+
+### Nombre complexe
+#z = x + iy comme z = complex(x,y)
+
+a=2-1j
+b=-10-5j
+c=3+11j
+print("a=", a, ", b=", b, ", c=", c)
+D=b**2-4*a*c
+print("Discriminant=", D)
+d=D**(1/2)
+print("delta=", d)
+z1=(-b-d)/(2*a)
+z2=(-b+d)/(2*a)
+print("solutions de l'équation az**2+bz+c=0")
+print("z1=",z1)
+print("z2",z2)
+
+x="Boileau a dit : \n"
+y1="\t Hâtez-vous lentement, et sans perdre courage, \n"
+y2="\t Vingt fois sur le métier remetez votre ouvrage :\n"
+y3="\t Polissez-le sans cesse et le repolissez ! \n"
+print(x, y1, y2, y3)
+
+
+x=3
+y=5
+print("x =", x , end=" & ")
+print("y = ",y)
